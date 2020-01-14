f4b9201fcb2ddd46be251112a1e45549f12ddedd
Martijn Eisses <martijn-eisses@live.nl>
2020-01-14 16:12:37 +0100

    Added the TextFromFile.java and added a textfile

---
+++ TextFromFile.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+// Reads the file and saves it to a list
+public class TextFromFile {
+
+    Map<Integer, String> textLines = new HashMap<Integer, String>();
+
+    public TextFromFile() {
+        GetTextFromFile("textfile.txt");
+    }
+
+    private void GetTextFromFile(String path) {
+        try {
+            File file = new File(path); // Creates a new file instance
+            FileReader fr = new FileReader(file); // Reads the file
+            BufferedReader br = new BufferedReader(fr); // Creates a buffering character input stream
+            StringBuffer sb = new StringBuffer(); // Constructs a string buffer with no
+            // characters
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                SplitString(line);
+            }
+            fr.close(); // Closes the stream and release the resources
+        } catch (IOException e) {
+            // Sends an error when the file could not be read
+            e.printStackTrace();
+        }
+    }
+
+    // Splits the string and adds it to the map
+    private void SplitString(String line) {
+        String[] splittedLine = line.split("||");
+        AddLineToDictionary(Integer.parseInt(splittedLine[0]), splittedLine[1]);
+    }
+
+    private void AddLineToDictionary(Integer key, String value) {
+        if (textLines.containsKey(key)) {
+            System.out.println(key + "  Already exists");
+        }
+        textLines.put(key, value);
+    }
+
+    public String GetLine(Integer key) {
+        if (textLines.containsKey(key)) {
+            return textLines.get(key);
+        } else
+            return "INCORRECT KEY";
+    }
+}
